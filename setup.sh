7a229a6204699de4191bf40ff7a2f04b1251dac4
Michael C <michael@mchang.name>
2021-11-22 22:32:57 -0500

    change path

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # add key to apkfile
-wget -q https://alpine.mchang.icu/michael@mchang.name.rsa.pub -P /etc/apk/keys/
+wget -q https://pub.mchang.icu/alpine/michael@mchang.name.rsa.pub -P /etc/apk/keys/
 # add repository to repositories
-echo "https://alpine.mchang.icu/repo" >> /etc/apk/repositories
+echo "https://pub.mchang.icu/alpine/repo/" >> /etc/apk/repositories
